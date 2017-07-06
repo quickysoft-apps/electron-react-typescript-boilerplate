873b8559a24fe20f4c3e7a99f0440046812693e3
Lawrence <neurosoup@gmail.com>
2017-07-06 14:11:36 +0200

    yakapa client middleware

--- app/api/yakapa/client.ts
+++ app/api/yakapa/client.ts
@@ -2,7 +2,6 @@ import * as Log from 'electron-log';
 import * as SocketIo from 'socket.io-client';
 import * as LZString from 'lz-string';
 import * as Faker from 'faker';
-import Events from './events';
 
 const SOCKET_SERVER_URL = 'https://mprj.cloudapp.net'
 
@@ -12,7 +11,17 @@ interface SocketMessage {
   Message: string;
 }
 
+export namespace SocketEvent {
+  const EVENT_PREFIX: string = 'yakapa';
+  export const CHAT: string = `${EVENT_PREFIX}/chat`;
+  export const RESULT: string = `${EVENT_PREFIX}/result`;
+  export const EXECUTE_SCRIPT: string = `${EVENT_PREFIX}/executescript`;
+  export const AUTHENTICATION: string = `${EVENT_PREFIX}/authentication`;
+  export const AUTHENTICATED: string = `${EVENT_PREFIX}/authenticated`;
+}
+
 export class YakapaClient {
+
   private _socket: SocketIOClient.Socket;
   private _isAuthenticated: boolean = false;
   private _nickname: string;
@@ -20,9 +29,9 @@ export class YakapaClient {
   constructor() {
     this._socket = SocketIo.connect(SOCKET_SERVER_URL, { reconnection: true });
     this._socket.on('connect', () => { this.connect() })
-    this._socket.on(Events.AUTHENTICATED, (socketMessage: SocketMessage) => { this.authenticate(socketMessage) })
-    this._socket.on(Events.CHAT, async (socketMessage: SocketMessage) => { await this.understand(socketMessage) })
-    this._socket.on(Events.EXECUTE_SCRIPT, async (socketMessage: SocketMessage) => { await this.executeScript(socketMessage) })
+    this._socket.on(SocketEvent.AUTHENTICATED, (socketMessage: SocketMessage) => { this.authenticate(socketMessage) })
+    this._socket.on(SocketEvent.CHAT, async (socketMessage: SocketMessage) => { await this.understand(socketMessage) })
+    this._socket.on(SocketEvent.EXECUTE_SCRIPT, async (socketMessage: SocketMessage) => { await this.executeScript(socketMessage) })
   }
 
   getJson(json: any): any {
@@ -50,15 +59,15 @@ export class YakapaClient {
     return true
   }
 
-  private emit(event: string = Events.RESULT, payload?: string, to?: string): void {
+  private emit(event: string = SocketEvent.RESULT, payload?: string, to?: string): void {
     const compressed = payload != null ? LZString.compressToUTF16(payload) : null
 
     const socketMessage = {
       From: 'TODO-TAG',
       FromNickname: 'TODO-nickname',
       To: to,
-      Result: event === Events.RESULT ? compressed : null,
-      Message: event === Events.CHAT ? compressed : null
+      Result: event === SocketEvent.RESULT ? compressed : null,
+      Message: event === SocketEvent.CHAT ? compressed : null
     }
 
     this._socket.emit(event, socketMessage)
@@ -66,7 +75,7 @@ export class YakapaClient {
 
   private connect(): void {
     Log.info('Connecté à', SOCKET_SERVER_URL)
-    this.emit(Events.AUTHENTICATION)
+    this.emit(SocketEvent.AUTHENTICATION)
   }
 
   private authenticate(socketMessage: SocketMessage): void {
@@ -81,7 +90,7 @@ export class YakapaClient {
       const decompressed = LZString.decompressFromUTF16(socketMessage.Message);
       Log.info(`Received chat message ${decompressed}`);
       const emitter = socketMessage.From;
-      this.emit(Events.CHAT, Faker.lorem.sentence(15), emitter);
+      this.emit(SocketEvent.CHAT, Faker.lorem.sentence(15), emitter);
       resolve();
     });
   }
--- app/store/yakapaClientMiddleware.ts
+++ app/store/yakapaClientMiddleware.ts
@@ -1,1 +1,25 @@
-import * as Actions from '../actions/chat'
+import * as actions from '../actions/chat';
+import { YakapaClient } from '../api/yakapa/client';
+ 
+let client: YakapaClient
+
+export function yakapaClientMiddleware(store: Store<any>) {
+  return next => action => {
+    const result = next(action);
+ 
+    if (socket && action.type === actions.ADD_MESSAGE) {
+      let messages = store.getState().messages;
+      socket.emit('message', messages[messages.length -1]);
+    }
+ 
+    return result;
+  };
+}
+ 
+export default function (store) {
+  socket = io.connect(`${location.protocol}//${location.host}`);
+ 
+  socket.on('message', data => {
+    store.dispatch(actions.addResponse(data));
+  });
+}
