dc1301924bc14f4dafa7dfe4d37f12366872bb2d
Lawrence Zémour <neurosoup@gmail.com>
2017-07-09 19:27:34 +0200

    removed comments

--- app/store/yakapaClientMiddleware.ts
+++ app/store/yakapaClientMiddleware.ts
@@ -5,20 +5,6 @@ import { YakapaEvent, YakapaClient, YakapaMessage } from '../api/yakapaClient';
 
 const client: YakapaClient = new YakapaClient();
 
-// interface YakapaDispatch extends Dispatch<YakapaMessage> {
-//   (action: IActionWithPayload<YakapaMessage>): IActionWithPayload<YakapaMessage>;
-// }
-
-// export function yakapaClientMiddleware(): Middleware {
-//   return (api: MiddlewareAPI<State>) => (next: Dispatch<State>) => (dispatch : YakapaDispatch) => (action: IActionWithPayload<YakapaMessage>) => {
-//     const result = next(action);
-//     if (action.type === actions.send) {
-//       client.emit(YakapaEvent.CHAT, action.payload.message, action.payload.from);
-//     }
-//     return result;
-//   }
-// }
-
 export function yakapaClientMiddleware(): Redux.Middleware {
   return (api: Redux.MiddlewareAPI<any>) => (next: Redux.Dispatch<any>) => <A extends IActionWithPayload<YakapaMessage>>(action: A) => {
     const result = next(action);
