4fbee2bae923cbae9699155901ae5782659f15ec
Lawrence <neurosoup@gmail.com>
2017-07-12 14:03:22 +0200

    material-ui

--- app/containers/Root.tsx
+++ app/containers/Root.tsx
@@ -6,6 +6,10 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import Routes from '../routes';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import theme from '../theme'
+
 interface IRootType {
   store: Redux.Store<any>;
   history: History
@@ -14,9 +18,11 @@ interface IRootType {
 export default function Root({ store, history }: IRootType) {
   return (
     <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Routes />
-      </ConnectedRouter>
+      <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+        <ConnectedRouter history={history}>
+          <Routes />
+        </ConnectedRouter>
+      </MuiThemeProvider>
     </Provider>
   );
 }
