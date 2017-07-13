a8ac64c768800f82fa7320a2d0311f755b52d72a
Lawrence <neurosoup@gmail.com>
2017-07-13 14:09:23 +0200

    material-ui

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
 
-let styles = require('./Counter.scss');
+//let styles = require('./Counter.scss');
 
 export interface IProps extends RouteComponentProps<any> {
   increment(): void,
@@ -17,23 +17,23 @@ export class Counter extends React.Component<IProps> {
     const { increment, incrementIfOdd, incrementAsync, decrement, counter } = this.props;
     return (
       <div>
-        <div className={styles.backButton} data-tid="backButton">
+        <div data-tid="backButton">
           <Link to="/">
             <i className="fa fa-arrow-left fa-3x" />
           </Link>
         </div>
-        <div className={`counter ${styles.counter}`} data-tid="counter">
+        <div data-tid="counter">
           {counter}
         </div>
-        <div className={styles.btnGroup}>
-          <button className={styles.btn} onClick={increment} data-tclass="btn">
+        <div>
+          <button onClick={increment} data-tclass="btn">
             <i className="fa fa-plus" />
           </button>
-          <button className={styles.btn} onClick={decrement} data-tclass="btn">
+          <button onClick={decrement} data-tclass="btn">
             <i className="fa fa-minus" />
           </button>
-          <button className={styles.btn} onClick={incrementIfOdd} data-tclass="btn">odd</button>
-          <button className={styles.btn} onClick={() => incrementAsync()} data-tclass="btn">async</button>
+          <button onClick={incrementIfOdd} data-tclass="btn">odd</button>
+          <button onClick={() => incrementAsync()} data-tclass="btn">async</button>
         </div>
       </div>
     );
--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-
-let styles = require('./Home.scss');
+import { FlatButton } from 'material-ui'
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <div className={styles.container} data-tid="container">
+        <div data-tid="container">
           <h2>Yakapa 0.4.0</h2>
-          <Link to="/counter">to Counter</Link>
+          <FlatButton>
+            <Link to="/counter">to Counter</Link>
+          </FlatButton>
         </div>
       </div>
     );
