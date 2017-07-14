1fbd63ef74ce9d8cbf7d02673420cdfb20bf71dc
Lawrence Zémour <neurosoup@gmail.com>
2017-07-14 18:20:11 +0200

    heart

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { red700, red800, grey800 } from 'material-ui/styles/colors';
+import { red500, red800, grey800 } from 'material-ui/styles/colors';
+import * as Log from 'electron-log'
 
 const containerStyle = {
   textAlign: "center"
@@ -7,7 +8,7 @@ const containerStyle = {
 
 const aliveStyle = { fill: red800 }
 const asleepStyle = { fill: grey800 }
-const aliveHoverStyle = { fill: red700 }
+const aliveHoverStyle = { fill: red500 }
 
 const svgStyle = {
   width: "75%",
@@ -15,22 +16,32 @@ const svgStyle = {
   transition: "fill 0.5s ease"
 }
 
-export interface IState {
+interface Props { }
+
+interface State {
   hover: boolean
 }
 
-export default class Home extends React.Component<any, IState> {
+export default class Home extends React.Component<Props, State> {
 
-  constructor(props: any) {
+  constructor(props: Props) {
     super(props);
-    this.state = { hover: false };
+    this.state = { hover: false };    
   }
 
-  toggleHover = () => {
+  public toggleHover() {        
     this.setState({ hover: !this.state.hover })
   }
 
-  render() {
+  public componentDidMount() {
+    Log.debug('mounted');
+  }
+
+  public componentWillUnmount() {
+    Log.debug('did unmout');
+  }
+
+  public render() {
 
     const aliveStyleWithHover = this.state.hover ? aliveHoverStyle : aliveStyle;
     const statusStyle = true ? aliveStyleWithHover : asleepStyle;
@@ -38,7 +49,7 @@ export default class Home extends React.Component<any, IState> {
 
     return (
       <div style={containerStyle}>
-        <svg onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} viewBox="0 0 24 24" style={svgStyleWithStatus}>
+        <svg onMouseEnter={e => this.toggleHover()} onMouseLeave={e => this.toggleHover()} viewBox="0 0 24 24" style={svgStyleWithStatus}>
           <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
         </svg>
 
