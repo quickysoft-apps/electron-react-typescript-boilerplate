bd8a6f395a3ea0fb67ca04dcd7bce242fbed4320
Lawrence <neurosoup@gmail.com>
2017-07-20 14:34:34 +0200

    svg anim

--- app/components/Heart.tsx
+++ app/components/Heart.tsx
@@ -1,37 +1,12 @@
 import * as React from 'react';
 import { red500, red800, orange800, grey800 } from 'material-ui/styles/colors';
+import * as Snap from 'snapsvg';
 import 'typeface-roboto';
 
 import { AgentStatus } from '../reducers/home'
 
 
-/*
-$ ->
-  mapsvg = Snap $("svg")[0]
-  setTimeout ( =>
-    path = mapsvg.select('#California')
-    smallScale = new Snap.Matrix()
-    smallScale.scale(0.8,0.8, path.getBBox().cx, path.getBBox().cy)
-    path.animate {transform: smallScale}, 1000, mina.elastic
-  ), 500
-  mapsvg.hover ((e)->
-    s = Snap e.target
-    p = s.select('path')
-    midScale = new Snap.Matrix()
-    midScale.scale(0.9, 0.9, p.getBBox().cx, p.getBBox().cy)
-    p.animate {
-      transform: midScale
-    }, 1000, mina.elastic
-  ), ((e)->
-    s = Snap e.target
-    p = s.select('path')
-    midScale = new Snap.Matrix()
-    midScale.scale(0.8, 0.8, p.getBBox().cx, p.getBBox().cy)
-    p.animate {
-      transform: midScale
-    }, 1000, mina.elastic
-  )
-*/
+
 
 
 interface Props {
@@ -70,10 +45,42 @@ export class Heart extends React.Component<Props, State> {
     this.state = { hover: false };
   }
 
+  public componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>) {
+   
+    const svg = Snap('#svg');
+    setTimeout(() => {
+      const path = svg.select('path');
+      const smallScale = new Snap.Matrix();
+      smallScale.scale(0.8, 0.8, path.getBBox().cx, path.getBBox().cy);
+      path.animate({ transform: smallScale }, 1000, mina.elastic);
+    }, 500);
+
+    svg.hover(
+      function (e: MouseEvent) {
+        const s = Snap(e.target as SVGElement);
+        const p = s.select('path');
+        const midScale = new Snap.Matrix();
+        midScale.scale(0.9, 0.9, p.getBBox().cx, p.getBBox().cy);
+        p.animate({
+          transform: midScale
+        }, 1000, mina.elastic);
+      },
+      function (e) {
+        const s = Snap(e.target as SVGElement);
+        const p = s.select('path');
+        const midScale = new Snap.Matrix();
+        midScale.scale(0.8, 0.8, p.getBBox().cx, p.getBBox().cy);
+        p.animate({
+          transform: midScale
+        }, 1000, mina.elastic);
+      }
+    );
+  }
+
   public render() {
 
     let statusStyle = {};
-    
+
     switch (this.props.status) {
       case AgentStatus.Alive:
         statusStyle = Heart.styles.alive;
@@ -85,14 +92,14 @@ export class Heart extends React.Component<Props, State> {
         statusStyle = Heart.styles.asleep;
         break;
     }
-    
+
     const iconStyle = { ...Heart.styles.icon, ...statusStyle }
 
     return (
       <div>
-        <svg onMouseEnter={e => this.toggleHover()} onMouseLeave={e => this.toggleHover()} viewBox="0 0 24 24" style={iconStyle}>
-              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-            </svg>
+        <svg id="svg" onMouseEnter={e => this.toggleHover()} onMouseLeave={e => this.toggleHover()} viewBox="0 0 24 24" style={iconStyle}>
+          <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+        </svg>
       </div >
 
     );
