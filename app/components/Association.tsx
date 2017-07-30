e0b7dd1c6b0c9f306c724007744b089f943d12b6
Lawrence Zémour <neurosoup@gmail.com>
2017-07-30 19:38:50 +0000

    association

--- app/components/Association.tsx
+++ app/components/Association.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { TextField } from 'material-ui'
+import { TextField, RaisedButton } from 'material-ui'
+import { grey300, white } from 'material-ui/styles/colors';
 
 
 export interface Props extends RouteComponentProps<any> {
@@ -11,18 +12,42 @@ export class Association extends React.Component<Props> {
 
   static styles = {
     container: {
-      textAlign: "center"
+      textAlign: "center",
+      fontFamily: "Roboto, sans-serif",
+      color: white
+    },
+    paragraph: {
+      textAlign: "left",
+      margin: 28,
+      color: grey300
+    },
+    button: {
+      margin: 12
     }
   };
 
   public render() {
-        
+
     return (
       <div style={Association.styles.container}>
+        <h2>Associer votre agent</h2>
+        <p style={Association.styles.paragraph}>Pour associer votre agent, il suffit de nous indiquer
+          votre email de contact tel que vous nous l'avez précédemment communiqué.</p>
+        <p style={Association.styles.paragraph}>Vous pouvez aussi identifier cet agent au sein de votre réseau en lui
+            donnant le nom que vous voulez, ou bien lui laisser celui que nous avons choisi pour vous.</p>
         <TextField
           hintText="votre.email@contact.fr"
-          floatingLabelText="Email"
+          floatingLabelText="Email de contact"
         />
+        <TextField
+          defaultValue="John Doe"
+          floatingLabelText="Identifiant"
+        />
+        <br/>
+        <br/>
+        <br/>
+        <RaisedButton style={Association.styles.button}>Annuler</RaisedButton>
+        <RaisedButton primary={true} style={Association.styles.button}>Associer</RaisedButton>
       </div >
 
     );
--- app/theme.ts
+++ app/theme.ts
@@ -1,14 +1,16 @@
+import darkBaseTheme from  'material-ui/styles/baseThemes/darkBaseTheme';
+
 import { grey800 } from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+const merge = require('lodash.merge');
 
-const muiTheme = getMuiTheme({
-  flatButton: {
-    "disabledTextColor": "rgba(255, 255, 255, 0.54)"
-  },
+const muiTheme = {  
   appBar: {
     height: 50,
     color: grey800
   }
-});
+};
 
-export default muiTheme;
+const theme = getMuiTheme(merge(darkBaseTheme, muiTheme))
+
+export default theme;
